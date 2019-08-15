bdb47053f88fc8d3448ad71efe634f02ab66c2b8
hmym <chika.hamayama@gmail.com>
2019-08-15 19:56:48 +0900

    switch分にリファクタリング途中

--- calcApp/ViewController.swift
+++ calcApp/ViewController.swift
@@ -13,6 +13,29 @@ class ViewController: UIViewController {
     var secondCount = 0
     var sumCount = 0
     
+    func sum() {
+        let firstCount = Int(firstCountField.text!)
+        let secondCount = Int(secondCountField.text!)
+        let plusSum = firstCount! + secondCount!
+        let minusSum = firstCount! - secondCount!
+        let kakeruSum = firstCount! * secondCount!
+        let waruSum = firstCount! / secondCount!
+        
+        switch sumCount {
+        case plusSum:
+            self.plusButtonAction(sumCount)
+            sumLabel.text = String(plusSum)
+        case minusSum:
+            sumLabel.text = String(minusSum)
+        case kakeruSum:
+            sumLabel.text = String(kakeruSum)
+        case waruSum:
+            sumLabel.text = String(waruSum)
+        default:
+            sumCount = 0
+        }
+    }
+    
     @IBOutlet weak var sumLabel: UILabel!
     
     @IBOutlet weak var firstCountField: UITextField!
@@ -20,17 +43,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondCountField: UITextField!
     
     @IBAction func plusButtonAction(_ sender: Any) {
-        let firstCount = Int(firstCountField.text!)
-        let secondCount = Int(secondCountField.text!)
-        let plusSum = firstCount! + secondCount!
-        sumLabel.text = String(plusSum)
+        print()
+        sum()
+        print()
+//        let firstCount = Int(firstCountField.text!)
+//        let secondCount = Int(secondCountField.text!)
+//        let plusSum = firstCount! + secondCount!
+//        sumLabel.text = String(plusSum)
     }
     
     @IBAction func minusButtonAction(_ sender: Any) {
-        let firstCount = Int(firstCountField.text!)
-        let secondCount = Int(secondCountField.text!)
-        let minusSum = firstCount! - secondCount!
-        sumLabel.text = String(minusSum)
+        sum()
+//        let firstCount = Int(firstCountField.text!)
+//        let secondCount = Int(secondCountField.text!)
+//        let minusSum = firstCount! - secondCount!
+//        sumLabel.text = String(minusSum)
     }
     
     @IBAction func delButtonAction(_ sender: Any) {
@@ -43,17 +70,19 @@ class ViewController: UIViewController {
     }
     
     @IBAction func kakeruButtonAction(_ sender: Any) {
-        let firstCount = Int(firstCountField.text!)
-        let secondCount = Int(secondCountField.text!)
-        let kakeruSum = firstCount! * secondCount!
-        sumLabel.text = String(kakeruSum)
+        sum()
+//        let firstCount = Int(firstCountField.text!)
+//        let secondCount = Int(secondCountField.text!)
+//        let kakeruSum = firstCount! * secondCount!
+//        sumLabel.text = String(kakeruSum)
     }
     
     @IBAction func waruButtonAction(_ sender: Any) {
-        let firstCount = Int(firstCountField.text!)
-        let secondCount = Int(secondCountField.text!)
-        let waruSum = firstCount! / secondCount!
-        sumLabel.text = String(waruSum)
+        sum()
+//        let firstCount = Int(firstCountField.text!)
+//        let secondCount = Int(secondCountField.text!)
+//        let waruSum = firstCount! / secondCount!
+//        sumLabel.text = String(waruSum)
     }
     
     override func viewDidLoad() {
